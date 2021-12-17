f31f3dad00fe5d4d77fe050343c7335834e237c4
Per Kerssens <perkerssens@gmail.com>
2021-12-17 15:52:33 +0100

    Added 2nd joint, robot still not fixed..

--- src/cherry_bot_gazebo/launch/cherry_bot.launch.py
+++ src/cherry_bot_gazebo/launch/cherry_bot.launch.py
@@ -41,13 +41,13 @@ def generate_launch_description():
       parameters=[{'use_sim_time': use_sim_time, 'robot_description': robot_desc}],
       arguments=[urdf]),
 
-   #Node(
-   #   package='joint_state_publisher',
-   #   executable='joint_state_publisher',
-   #   name='joint_state_publisher',
-   #   output='screen',
-   #   parameters=[{'use_sim_time': use_sim_time}]
-   #   ),
+   Node(
+      package='joint_state_publisher_gui',
+      executable='joint_state_publisher_gui',
+      name='joint_state_publisher',
+      output='screen',
+      parameters=[{'use_sim_time': use_sim_time}]
+      ),
 
     
     Node(
